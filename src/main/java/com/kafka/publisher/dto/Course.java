09e48b6eaa896eea79c5da695eae0099e8f4cb12
nadeemrain <nadeem.nsr03@yahoo.com>
2026-07-04 22:16:37 +0530

    make the changes in the configuration

--- src/main/java/com/kafka/publisher/dto/Course.java
+++ src/main/java/com/kafka/publisher/dto/Course.java
@@ -1,10 +1,19 @@
 package com.kafka.publisher.dto;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Course {
+    @JsonProperty
     private String name;
+    @JsonProperty
     private int duration;
+
+    public Course() {}
+
+    public Course(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
 
     public String getName() {
         return name;
@@ -20,5 +29,13 @@ public class Course {
 
     public void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", duration=" + duration +
+                '}';
     }
 }
